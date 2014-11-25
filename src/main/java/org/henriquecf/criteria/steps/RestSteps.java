e1b3ab15d0d04643c8ef76180a7dd94b66b9a9fe
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-11-25 17:25:39 -0200

    Finished creating the steps. Now need to implement them properly.

--- src/main/java/org/henriquecf/criteria/steps/RestSteps.java
+++ src/main/java/org/henriquecf/criteria/steps/RestSteps.java
@@ -2,6 +2,7 @@ package org.henriquecf.criteria.steps;
 
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.When;
+import org.jbehave.core.annotations.Then;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class RestSteps {
@@ -17,9 +18,23 @@ public class RestSteps {
     }
 
     @When("eu der um $metodo na url $url")
-    public void setMetodoEUrl(String metodo, String url) {
+    public void sendMetodoEUrl(String metodo, String url) {
         org.junit.Assert.assertThat(metodo, equalTo("get"));
         org.junit.Assert.assertThat(url, equalTo("http://google.com"));
     }
-    
+
+    @Then("eu deveria receber os seguintes headers: $headers")
+    public void checkHeaders(String headers) {
+        org.junit.Assert.assertThat(headers, equalTo("[]"));
+    }
+
+    @Then("o seguinte corpo JSON: $json")
+    public void checkJson(String json){
+        org.junit.Assert.assertThat(json, equalTo("[]"));
+    }
+
+    @Then("o codigo de status $status")
+    public void checkStatus(String status) {
+        org.junit.Assert.assertThat(status, equalTo("200"));
+    }
 }
--- src/main/resources/org/henriquecf/criteria/stories/exemplo_requisicao_http.story
+++ src/main/resources/org/henriquecf/criteria/stories/exemplo_requisicao_http.story
@@ -2,12 +2,8 @@ Scenario: consulta HTTP exemplo
 Given os seguintes headers: []
 And o seguinte corpo JSON: []
 When eu der um get na url http://google.com
-Then eu deveria receber os seguintes headers:
-  """
-  """
-And o seguinte corpo JSON:
-  """
-  """
+Then eu deveria receber os seguintes headers: []
+And o seguinte corpo JSON: []
 And o codigo de status 200
 
 
